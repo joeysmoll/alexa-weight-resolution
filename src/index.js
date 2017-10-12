1c42c5c51e8956a4f04aa798dcebca3e607dd434
Joey Smoll <jsmoll91@gmail.com>
2017-10-11 22:55:04 -0700

    starting weight writes to dynamoDB

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const APP_ID = 'amzn1.ask.skill.9f38e666-f9d0-4970-892d-5b2793cef971';
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
+
+    alexa.dynamoDBTableName = 'weightTable';
+
     alexa.APP_ID = APP_ID;
     alexa.registerHandlers(handlers);
     alexa.execute();
@@ -14,7 +17,6 @@ exports.handler = function(event, context, callback) {
 var handlers = {
     'LaunchRequest': function() {
         if (Object.keys(this.attributes).length === 0) {
-            this.attributes['endedSessionCount'] = 0;
             this.attributes['setStartingWeight'] = null;
             this.emit('LaunchIntent');
         } else {
@@ -31,20 +33,17 @@ var handlers = {
         var startDecimal = this.event.request.intent.slots.StartWeightDecimal.value;
         if (startDecimal == null) {
             this.attributes["startingWeight"] = parseInt(this.event.request.intent.slots.StartWeight.value);
-            var startWeight = this.attributes["startingWeight"];
             console.log('starting weight: ' + startWeight);
 
             //change to :ask and add yes/no to confirm weight.
-            this.emit(':tell', 'Your starting weight is ' + startWeight + 'pounds.');
+            this.emit(':tell', 'Your starting weight is ' + this.attributes["startingWeight"] + '-pounds.');
         } else {
-            this.attributes["startingWeight"] = parseInt(this.event.request.intent.slots.StartWeight.value);
-            this.attributes["startingDecimal"] = parseInt(this.event.request.intent.slots.StartWeightDecimal.value);
-            var weightString = this.attributes["startingWeight"] + "." + this.attributes["startingDecimal"];
-            var startWeight = parseInt(weightString);
-            console.log('starting weight: ' + startWeight);
+            this.attributes["startingWeight"] = parseFloat(this.event.request.intent.slots.StartWeight.value + "." + this.event.request.intent.slots.StartWeightDecimal.value);
+            console.log('starting weight: ' + this.attributes["startingWeight"]);
 
             //change to :ask and add yes/no to confirm weight.
-            this.emit(':tell', 'It looks like your starting weight is ' + this.attributes["startingWeight"] + 'point' + this.attributes["startingDecimal"] + '-pounds.');
+            this.emit(':tell', 'It looks like your starting weight is ' + this.attributes["startingWeight"] + '-pounds.');
         }
     }
+
 };
