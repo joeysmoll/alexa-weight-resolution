2cf83ce9ec3b750fb4f7cabf70ecd810c97de04b
Joey Smoll <jsmoll91@gmail.com>
2017-10-16 13:23:20 -0700

    Started Record weight

--- src/index.js
+++ src/index.js
@@ -10,40 +10,71 @@ exports.handler = function(event, context, callback) {
     alexa.dynamoDBTableName = 'weightTable';
 
     alexa.APP_ID = APP_ID;
-    alexa.registerHandlers(handlers);
+    alexa.registerHandlers(handlers, recordWeightHandlers);
     alexa.execute();
 };
 
+var STATES = {
+    RECORDWEIGHT: '_RECORDWEIGHTMODE'
+};
+
 var handlers = {
+    //when skill is launched with no intents
     'LaunchRequest': function() {
         if (Object.keys(this.attributes).length === 0) {
             this.attributes['setStartingWeight'] = null;
             this.emit('LaunchIntent');
         } else {
             //route to record weight intent
+            this.emit('LaunchIntent');
         }
     },
 
     'LaunchIntent': function() {
-        this.emit(':ask', 'Hello, I am Weight Bot. I noticed you have not set up a starting weight. Your starting weight will be used to track long term weight changes. Please say the number of pounds you currently weigh.');
+        if (this.attributes['setStartingWeight'] == null) {
+            this.emit(':ask', 'Hello, I am Weight Bot. I noticed you have not set up a starting weight. Your starting weight will be used to track long term weight changes. Please say the number of pounds you currently weigh.');
+        } else {
+            this.handler.state = STATES.RECORDWEIGHT;
+            this.emit(':ask', 'How much do you weight today?');
+        }
     },
 
     'RecordStartingWeight': function() {
-        var startPounds = this.event.request.intent.slots.StartWeight.value;
         var startDecimal = this.event.request.intent.slots.StartWeightDecimal.value;
         if (startDecimal == null) {
             this.attributes["startingWeight"] = parseInt(this.event.request.intent.slots.StartWeight.value);
-            console.log('starting weight: ' + startWeight);
+            console.log('starting weight: ' + this.attributes["startingWeight"]);
 
-            //change to :ask and add yes/no to confirm weight.
-            this.emit(':tell', 'Your starting weight is ' + this.attributes["startingWeight"] + '-pounds.');
+            this.emit(':ask', 'Your starting weight is ' + this.attributes["startingWeight"] + '-pounds. Is this correct?');
         } else {
             this.attributes["startingWeight"] = parseFloat(this.event.request.intent.slots.StartWeight.value + "." + this.event.request.intent.slots.StartWeightDecimal.value);
             console.log('starting weight: ' + this.attributes["startingWeight"]);
 
-            //change to :ask and add yes/no to confirm weight.
-            this.emit(':tell', 'It looks like your starting weight is ' + this.attributes["startingWeight"] + '-pounds.');
+            this.emit(':ask', 'It sounds like your starting weight is ' + this.attributes["startingWeight"] + '-pounds. Is this correct?');
         }
-    }
+    },
 
+    'AMAZON.YesIntent': function() {
+        this.attributes['setStartingWeight'] = true;
+        this.emit(':tell', 'Your starting weight of ' + this.attributes["startingWeight"] + ' pounds has been saved.')
+    },
+
+    'AMAZON.NoIntent': function() {
+        this.emit(':ask', 'Please say the number of pounds you currently weigh.');
+    }
 };
+
+var recordWeightHandlers = Alexa.CreateStateHandler(STATES.RECORDWEIGHT, {
+
+    //start with different intent?
+    // 'LaunchWeight': function() {
+    //     this.attributes['Weight'] = null;
+    //     this.emit(':ask', 'How much do you weight today?');
+    // },
+
+    'RecordWeight': function() {
+        this.attributes['Weight'] = this.event.request.intent.slots.Weight.value;
+        //this.attributes['Weight'] = 150;
+        this.emit(':tell', 'Today you weigh, ' + this.attributes['Weight']);
+    }
+});
