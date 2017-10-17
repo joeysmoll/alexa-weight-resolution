0b08b7d9cf5d752142d4b67964a6760b0006c2db
Joey Smoll <jsmoll91@gmail.com>
2017-10-16 21:29:36 -0700

    Added daily weight recorder

--- src/index.js
+++ src/index.js
@@ -18,6 +18,22 @@ var STATES = {
     RECORDWEIGHT: '_RECORDWEIGHTMODE'
 };
 
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth() + 1; //January is 0!
+var monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
+var yyyy = today.getFullYear();
+if (dd < 10) {
+    dd = '0' + dd
+}
+if (mm < 10) {
+    mm = '0' + mm
+}
+var currentDay = mm + '/' + (dd - 1) + '/' + yyyy;
+
+
 var handlers = {
     //when skill is launched with no intents
     'LaunchRequest': function() {
@@ -35,12 +51,14 @@ var handlers = {
             this.emit(':ask', 'Hello, I am Weight Bot. I noticed you have not set up a starting weight. Your starting weight will be used to track long term weight changes. Please say the number of pounds you currently weigh.');
         } else {
             this.handler.state = STATES.RECORDWEIGHT;
-            this.emit(':ask', 'How much do you weight today?');
+            this.emitWithState('LaunchWeight');
+            //this.emit(':ask', 'How much do you weight today?');
         }
     },
 
     'RecordStartingWeight': function() {
         var startDecimal = this.event.request.intent.slots.StartWeightDecimal.value;
+        this.attributes['startWeightDate'] = currentDay;
         if (startDecimal == null) {
             this.attributes["startingWeight"] = parseInt(this.event.request.intent.slots.StartWeight.value);
             console.log('starting weight: ' + this.attributes["startingWeight"]);
@@ -56,7 +74,7 @@ var handlers = {
 
     'AMAZON.YesIntent': function() {
         this.attributes['setStartingWeight'] = true;
-        this.emit(':tell', 'Your starting weight of ' + this.attributes["startingWeight"] + ' pounds has been saved.')
+        this.emit(':tell', 'Your starting weight of ' + this.attributes["startingWeight"] + ' pounds has been saved.');
     },
 
     'AMAZON.NoIntent': function() {
@@ -65,16 +83,37 @@ var handlers = {
 };
 
 var recordWeightHandlers = Alexa.CreateStateHandler(STATES.RECORDWEIGHT, {
+            //start with different intent?
+            'LaunchWeight': function() {
+                //this.attributes['Weight'] = null;
+                this.emit(':ask', 'How much do you weigh today?');
+            },
 
-    //start with different intent?
-    // 'LaunchWeight': function() {
-    //     this.attributes['Weight'] = null;
-    //     this.emit(':ask', 'How much do you weight today?');
-    // },
+            'RecordWeight': function() {
+                var weightDecimal = this.event.request.intent.slots.WeightDecimal.value;
+                if (weightDecimal == null) {
+                    this.attributes['Weight'] = this.event.request.intent.slots.Weight.value;
+                    this.emit(':ask', 'Today you weigh, ' + this.attributes['Weight'] + " pounds. Is this correct?");
+                } else {
+                    this.attributes["Weight"] = parseFloat(this.event.request.intent.slots.Weight.value + "." + this.event.request.intent.slots.WeightDecimal.value);
+                    this.emit(':ask', 'It sounds like today you weigh, ' + this.attributes['Weight'] + " pounds. Is this correct?");
+                }
+            },
 
-    'RecordWeight': function() {
-        this.attributes['Weight'] = this.event.request.intent.slots.Weight.value;
-        //this.attributes['Weight'] = 150;
-        this.emit(':tell', 'Today you weigh, ' + this.attributes['Weight']);
-    }
-});
+            'AMAZON.YesIntent': function() {
+                if (this.attributes['startingWeight'] >= this.attributes['Weight']) {
+                    this.attributes['weightChange'] = Math.round((this.attributes['startingWeight'] - this.attributes['Weight']) * 10) / 10;
+                    this.emit(':tell', 'You have lost ' + this.attributes['weightChange'] + ' pounds since ' + this.attributes['startWeightDate']);
+                    //this.emit(':tell', 'You have lost ' + this.attributes['weightChange'] + 'pounds since ' + <say-as interpret-as = "date"> this.attributes['startWeightDate'] </say-as>);
+                    }
+                    else {
+                        this.attributes['weightChange'] = Math.round((this.attributes['Weight'] - this.attributes['startingWeight']) * 10) / 10;
+                        this.emit(':tell', 'You have gained ' + this.attributes['weightChange'] + ' pounds since ' + this.attributes['startWeightDate']);
+                    }
+                    this.handler.state = null;
+                },
+
+                'AMAZON.NoIntent': function() {
+                    this.emit(':ask', 'Please say the number of pounds you currently weigh.');
+                }
+            });
