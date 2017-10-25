419ccdd341003100f943eda9168ac0d881393324
Joey Smoll <jsmoll91@gmail.com>
2017-10-24 17:27:25 -0700

    Updated date variables using moment.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 var Alexa = require('alexa-sdk');
 var AWS = require('aws-sdk');
 AWS.config.region = 'us-east-1';
+var moment = require('moment-timezone');
 
-//enter app_id
 const APP_ID = 'amzn1.ask.skill.9f38e666-f9d0-4970-892d-5b2793cef971';
 
 exports.handler = function(event, context, callback) {
@@ -14,49 +14,47 @@ exports.handler = function(event, context, callback) {
     alexa.execute();
 };
 
-var today = new Date();
-var dd = today.getDate();
-var mm = today.getMonth() + 1; //January is 0!
-var yyyy = today.getFullYear();
-if (dd < 10) {
-    dd = '0' + dd
-}
-if (mm < 10) {
-    mm = '0' + mm
-}
-var currentDay = mm + '/' + (dd - 1) + '/' + yyyy;
+// var today = new Date();
+// var dd = today.getDate();
+// var mm = today.getMonth() + 1; //January is 0!
+// var yyyy = today.getFullYear();
+// if (dd < 10) {
+//     dd = '0' + dd
+// }
+// if (mm < 10) {
+//     mm = '0' + mm
+// }
+// var currentDay = mm + '/' + (dd - 1) + '/' + yyyy;
 
-function getLastWeek(){
-    var today = new Date();
-    var lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
-    return lastWeek ;
-}
-var lastWeek = getLastWeek();
-var lastWeekMonth = lastWeek.getMonth() + 1;
-var lastWeekDay = lastWeek.getDate();
-var lastWeekYear = lastWeek.getFullYear();
-var lastWeekDisplay = lastWeekMonth + "/" + lastWeekDay + "/" + lastWeekYear;
+// function getLastWeek(){
+//     var today = new Date();
+//     var lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
+//     return lastWeek ;
+// }
+// var lastWeek = getLastWeek();
+// var lastWeekMonth = lastWeek.getMonth() + 1;
+// var lastWeekDay = lastWeek.getDate();
+// var lastWeekYear = lastWeek.getFullYear();
+// var lastWeekDisplay = lastWeekMonth + "/" + lastWeekDay + "/" + lastWeekYear;
+
+var currentDay = moment().tz("America/Los_angeles").format('L');
+var lastWeekDisplay = moment().subtract(7, 'days').tz("America/Los_Angeles").format('L');
 
 var todayWeight = undefined;
 
-
-
 var handlers = {
-    //when skill is launched with no intents
     'LaunchRequest': function() {
         if (Object.keys(this.attributes).length === 0) {
             this.attributes['setStartingWeight'] = null;
-            //this.attributes['weightData'] = {weight:[], date:[]};
             this.emit('LaunchIntent');
         } else {
-            //route to record weight intent
             this.emit('LaunchIntent');
         }
     },
 
     'LaunchIntent': function() {
         if (this.attributes['setStartingWeight'] == null) {
-            this.emit(':ask', 'Hello, I am Weight Bot, your weight tracking companion. Its time to set up a starting weight. Your starting weight will be used to track long term weight changes. Please say the number of pounds you currently weigh.');
+            this.emit(':ask', 'Hello, I am Weight Bot, your weight tracking assistant. Its time to set up a starting weight. Your starting weight will be used to track long term weight changes. Please say the number of pounds you currently weigh.');
         } else {
             this.emit(':ask', 'How much do you weigh today?');
         }
@@ -64,18 +62,15 @@ var handlers = {
 
     'RecordWeight': function() {
         var weightDecimal = this.event.request.intent.slots.WeightDecimal.value;
-        //this.attributes['weightData'] = {weight:[], date:[]};
         if (weightDecimal == null && this.attributes['setStartingWeight'] == null) {
             this.attributes['weightData'] = {weight:[], date:[]};
             this.attributes['startWeightDate'] = currentDay;
             this.attributes["startingWeight"] = parseInt(this.event.request.intent.slots.Weight.value);
-            console.log('starting weight: ' + this.attributes["startingWeight"]);
             this.emit(':ask', 'It sounds like your starting weight is ' + this.attributes["startingWeight"] + '-pounds. Is this correct?');
         } else if (weightDecimal != null && this.attributes['setStartingWeight'] == null){
             this.attributes['weightData'] = {weight:[], date:[]};
             this.attributes['startWeightDate'] = currentDay;
             this.attributes["startingWeight"] = parseFloat(this.event.request.intent.slots.Weight.value + "." + this.event.request.intent.slots.WeightDecimal.value);
-            console.log('starting weight: ' + this.attributes["startingWeight"]);
             this.emit(':ask', 'It sounds like your starting weight is ' + this.attributes["startingWeight"] + '-pounds. Is this correct?');
         } else if(weightDecimal == null && this.attributes['setStartingWeight'] == true){
             todayWeight = parseInt(this.event.request.intent.slots.Weight.value);
@@ -92,12 +87,11 @@ var handlers = {
             this.attributes['setStartingWeight'] = true;
             this.attributes['weightData'].weight.push(this.attributes["startingWeight"]);
             this.attributes['weightData'].date.push(currentDay);
-            this.emit(':tell', 'Your starting weight of ' + this.attributes["startingWeight"] + ' pounds has been saved.');
+            this.emit(':tell', 'Your starting weight of ' + this.attributes["startingWeight"] + ' pounds has been saved. When recording your weight in the future you can quickly record your weight by saying something like, Alexa, tell weight bot, one hundred and seventy-five point two pounds. You can also ask for help, so you can get an idea of the different commands that you can say.');
         } else {
             //push daily weight
             this.attributes['lastWeekWeight'] = this.attributes['weightData'].weight[this.attributes['weightData'].date.indexOf(lastWeekDisplay)];
-            //this.attributes['lastWeekWeight'] = this.attributes['weightData'].weight[this.attributes['weightData'].date.indexOf(currentDay)];
-            //this.attributes['lastWeekWeight'] = 140;
+            //this.attributes['lastWeekWeight'] = 138;
             this.attributes['weightData'].weight.push(todayWeight);
             this.attributes['weightData'].date.push(currentDay);
             var weightChange = undefined;
@@ -109,10 +103,10 @@ var handlers = {
                 this.attributes['message'] = 'It looks like you have gained ' + weightChange + ' pounds since ' + this.attributes['startWeightDate'];
             }
             if(this.attributes['lastWeekWeight'] != null && this.attributes['lastWeekWeight'] >= todayWeight){
-                var weekWeightLoss = this.attributes['lastWeekWeight'] - todayWeight;
+                var weekWeightLoss = Math.round((this.attributes['lastWeekWeight'] - todayWeight) * 10) / 10;
                 this.attributes['message'] += '. And lost ' + weekWeightLoss + ' pounds since last week.'; 
             }else if (this.attributes['lastWeekWeight'] != null && todayWeight > this.attributes['lastWeekWeight']){
-                var weekWeightGain = todayWeight - this.attributes['lastWeekWeight'];
+                var weekWeightGain = Math.round((todayWeight - this.attributes['lastWeekWeight']) * 10) / 10;
                 this.attributes['message'] += '. And gained ' + weekWeightGain + ' pounds since last week.'; 
             }
             this.emit(':tell', this.attributes['message']);
@@ -129,7 +123,7 @@ var handlers = {
     },
 
     'ResetWeightBot': function(){
-        this.emit(':ask', 'Resetting weight bot will clear your weight data and will prompt you for your new starting weight. Please say, confirm reset to reset weight bot.');
+        this.emit(':ask', 'Resetting weight bot will clear your weight data and will prompt you for your new starting weight. Please say. confirm reset. this will reset weight bot.');
     },
 
     'ConfirmReset': function(){
