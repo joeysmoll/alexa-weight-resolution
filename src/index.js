d9894499697588f748e135e696a70da0671a5025
Joey Smoll <jsmoll91@gmail.com>
2017-10-25 18:52:28 -0700

    added VoiceLabs analytics

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 var Alexa = require('alexa-sdk');
 var AWS = require('aws-sdk');
 AWS.config.region = 'us-east-1';
+var VoiceLabs = require("voicelabs")('78c45990-b922-11a7-0b4c-02ddc10e4a8b');
 var moment = require('moment-timezone');
 
 const APP_ID = 'amzn1.ask.skill.9f38e666-f9d0-4970-892d-5b2793cef971';
@@ -54,9 +55,16 @@ var handlers = {
 
     'LaunchIntent': function() {
         if (this.attributes['setStartingWeight'] == null) {
-            this.emit(':ask', 'Hello, I am Weight Bot, your weight tracking assistant. Its time to set up a starting weight. Your starting weight will be used to track long term weight changes. Please say the number of pounds you currently weigh.');
+            var speechText = 'Hello, I am Weight Bot, your weight tracking assistant. Its time to set up a starting weight. Your starting weight will be used to track long term weight changes. Please say the number of pounds you currently weigh.';
+            VoiceLabs.track(this.event.session, 'LaunchIntent', null, speechText, (error, response) => {
+                console.log(error);
+                this.emit(':ask', speechText);
+            });
         } else {
-            this.emit(':ask', 'How much do you weigh today?');
+            var speechText = 'How much do you weigh today?';
+            VoiceLabs.track(this.event.session, 'LaunchIntent', null, speechText, (error, response) => {
+                this.emit(':ask', speechText);
+            });
         }
     },
 
@@ -66,18 +74,30 @@ var handlers = {
             this.attributes['weightData'] = {weight:[], date:[]};
             this.attributes['startWeightDate'] = currentDay;
             this.attributes["startingWeight"] = parseInt(this.event.request.intent.slots.Weight.value);
-            this.emit(':ask', 'It sounds like your starting weight is ' + this.attributes["startingWeight"] + '-pounds. Is this correct?');
+            var speechText = 'It sounds like your starting weight is ' + this.attributes["startingWeight"] + '-pounds. Is this correct?';
+            VoiceLabs.track(this.event.session, 'RecordWeight', this.attributes["startingWeight"], speechText, (error, response) => {
+                this.emit(':ask', speechText);
+            });
         } else if (weightDecimal != null && this.attributes['setStartingWeight'] == null){
             this.attributes['weightData'] = {weight:[], date:[]};
             this.attributes['startWeightDate'] = currentDay;
             this.attributes["startingWeight"] = parseFloat(this.event.request.intent.slots.Weight.value + "." + this.event.request.intent.slots.WeightDecimal.value);
-            this.emit(':ask', 'It sounds like your starting weight is ' + this.attributes["startingWeight"] + '-pounds. Is this correct?');
+            var speechText = 'It sounds like your starting weight is ' + this.attributes["startingWeight"] + '-pounds. Is this correct?';
+            VoiceLabs.track(this.event.session, 'RecordWeight', this.attributes["startingWeight"], speechText, (error, response) => {
+                this.emit(':ask', speechText);
+            });
         } else if(weightDecimal == null && this.attributes['setStartingWeight'] == true){
             todayWeight = parseInt(this.event.request.intent.slots.Weight.value);
-            this.emit(':ask', 'It sounds like your weight today is ' + todayWeight + '-pounds. Is this correct?');
+            var speechText = 'It sounds like your weight today is ' + todayWeight + '-pounds. Is this correct?';
+            VoiceLabs.track(this.event.session, 'RecordWeight', todayWeight, speechText, (error, response) => {
+                this.emit(':ask', speechText);
+            });
         } else{
             todayWeight = parseFloat(this.event.request.intent.slots.Weight.value + "." + this.event.request.intent.slots.WeightDecimal.value);
-            this.emit(':ask', 'It sounds like your weight today is ' + todayWeight + '-pounds. Is this correct?');
+            var speechText = 'It sounds like your weight today is ' + todayWeight + '-pounds. Is this correct?';
+            VoiceLabs.track(this.event.session, 'RecordWeight', todayWeight, speechText, (error, response) => {
+                this.emit(':ask', speechText);
+            });
         }
     },
 
@@ -87,7 +107,10 @@ var handlers = {
             this.attributes['setStartingWeight'] = true;
             this.attributes['weightData'].weight.push(this.attributes["startingWeight"]);
             this.attributes['weightData'].date.push(currentDay);
-            this.emit(':tell', 'Your starting weight of ' + this.attributes["startingWeight"] + ' pounds has been saved. When recording your weight in the future you can quickly record your weight by saying something like, Alexa, tell weight bot, one hundred and seventy-five point two pounds. You can also ask for help, so you can get an idea of the different commands that you can say.');
+            var speechText = 'Your starting weight of ' + this.attributes["startingWeight"] + ' pounds has been saved. When recording your weight in the future you can quickly record your weight by saying something along the lines of. Alexa, tell weight bot, one hundred and seventy-five point two pounds. Once I have enough weight data, I will tell you how much weight you have lost, or gained, since last week. You can also say help, after opening weight bot, to get a list of commands you can use with weight bot.';
+            VoiceLabs.track(this.event.session, 'AMAZON.YesIntent', 'Yes', speechText, (error, response) => {
+                this.emit(':tell', speechText);
+            });
         } else {
             //push daily weight
             this.attributes['lastWeekWeight'] = this.attributes['weightData'].weight[this.attributes['weightData'].date.indexOf(lastWeekDisplay)];
@@ -109,30 +132,48 @@ var handlers = {
                 var weekWeightGain = Math.round((todayWeight - this.attributes['lastWeekWeight']) * 10) / 10;
                 this.attributes['message'] += '. And gained ' + weekWeightGain + ' pounds since last week.'; 
             }
-            this.emit(':tell', this.attributes['message']);
+            var speechText = this.attributes['message'];
+            VoiceLabs.track(this.event.session, 'AMAZON.YesIntent', 'Yes', speechText, (error, response) => {
+                this.emit(':tell', speechText);
+            });
         }        
     },
 
     'AMAZON.NoIntent': function() {
-        this.emit(':ask', 'Please say the number of pounds you currently weigh.');
+        var speechText = 'Please say the number of pounds you currently weigh.';
+        VoiceLabs.track(this.event.session, 'AMAZON.NoIntent', 'No', speechText, (error, response) => {
+            this.emit(':ask', speechText);
+        }); 
     },
 
     'RepeatWeight': function(){
         var lastWeightDate = this.attributes['weightData'].date.slice(-1)[0];
-        this.emit(':tell', 'Your starting weight recorded on ' + this.attributes['startWeightDate'] + ' was ' + this.attributes["startingWeight"] + ' pounds. Your last recorded weight on ' + lastWeightDate + ' was ' + this.attributes['weightData'].weight.slice(-1)[0] + ' pounds.');
+        var speechText = 'Your starting weight recorded on ' + this.attributes['startWeightDate'] + ' was ' + this.attributes["startingWeight"] + ' pounds. Your last recorded weight on ' + lastWeightDate + ' was ' + this.attributes['weightData'].weight.slice(-1)[0] + ' pounds.';
+        VoiceLabs.track(this.event.session, 'RepeatWeight', 'What is my weight?', speechText, (error, response) => {
+            this.emit(':tell', speechText);
+        });
     },
 
     'ResetWeightBot': function(){
-        this.emit(':ask', 'Resetting weight bot will clear your weight data and will prompt you for your new starting weight. Please say. confirm reset. this will reset weight bot.');
+        var speechText = 'Resetting weight bot will clear your weight data and will prompt you for your new starting weight. Please say. confirm reset. this will reset weight bot.';
+        VoiceLabs.track(this.event.session, 'ResetWeightBot', 'Reset Weight Bot', speechText, (error, response) => {
+            this.emit(':ask', speechText);
+        }); 
     },
 
     'ConfirmReset': function(){
         delete this.attributes['setStartingWeight'];
         this.attributes['setStartingWeight'] == null;
-        this.emit('LaunchIntent');
+        var speechText = 'LaunchIntent';
+        VoiceLabs.track(this.event.session, 'ConfirmReset', 'Confirm Reset', speechText, (error, response) => {
+            this.emit(speechText);
+        }); 
     },
 
     'AMAZON.HelpIntent': function(){
-        this.emit(':tell', 'To quickly store your weight, you can say something like. Alexa, tell weight bot, one hundred and seventy-five pounds, or, Alexa, tell weight bot, my weight today is one hundred and seventy-five point five pounds. To hear your starting weight and your last recorded weight you can say. How much do I weigh? If the skill has not been launched yet, you can say. Alexa, ask weight bot, what is my weight? You can also reset your weight information by saying, reset weight bot.');
+        var speechText = 'To quickly store your weight, you can say. Alexa, tell weight bot, one hundred and seventy-five pounds, or. Alexa, tell weight bot, my weight today is one hundred and seventy-five point five pounds. To hear your starting weight, and your last recorded weight, you can say. Alexa, ask weight bot, how much do I weigh? To reset your weight information, you can say, reset weight bot.';
+        VoiceLabs.track(this.event.session, 'AMAZON.HelpIntent', 'Help', speechText, (error, response) => {
+            this.emit(':tell', speechText);
+        }); 
     }
 };
